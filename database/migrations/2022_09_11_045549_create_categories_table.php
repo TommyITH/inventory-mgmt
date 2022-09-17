66d1c88b26f630f1d8aea9455bd9a706f80f46e1
TommyITH <mgmgkyawwanakhing@gmail.com>
2022-09-17 10:39:05 +0630

    feat[telescope]: Setup local telescope

--- database/migrations/2022_09_11_045549_create_categories_table.php
+++ database/migrations/2022_09_11_045549_create_categories_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string("name", 50);
             $table->timestamp("created_at")->userCurrent();
-            $table->timestamp("updated_at")->useCurrentOnUpdate();
+            $table->timestamp("updated_at")->useCurrent()->useCurrentOnUpdate();
         });
     }
 
